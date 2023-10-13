8566df5e870655a976ce5fbece3d02a1616c0057
Trcmoe <88249678+Trcmoe@users.noreply.github.com>
2023-10-13 12:16:25 +0800

    v0.0.2

--- LME_for_Fabric.py
+++ LME_for_Fabric.py
@@ -1,18 +1,9 @@
-import os
-import json
 import glob
-import time
+import json
+import os
 
 
-def folderDetect():  # 判断文件夹名
-    current_dir = os.getcwd()
-    print(f"当前目录：{current_dir}")
-    if os.path.basename(current_dir) != "mods":
-        print("错误：该程序只能在“mods”文件夹下执行，请将程序移动至对应文件夹后重试。")
-        quit()
-
-
-def exportJars():
+def exportJSON():
     mod_list = []
     mod_files = glob.glob("*.jar")
     # 遍历所有jar包，提取数据
@@ -47,11 +38,12 @@ def formatJSON():
 
 
 def main():
-    folderDetect()
-    print("正在输出，请不要关闭本窗口。")
-    exportJars()
+    current_dir = os.getcwd()
+    print(f"当前目录：{current_dir}")
+    print("正在输出，请不要关闭本窗口，输出完毕后会自动关闭。")
+    exportJSON()
     formatJSON()
     print("输出完毕，现在可关闭本窗口。")
-    time.sleep(10)
+
 
 main()
